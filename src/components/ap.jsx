f03ee75b38d5edbb5ddf3246c52a5d9481353895
Andrii2024 <krovlya2005@gmail.com>
2024-02-16 17:00:26 +0200

    UPDATE17:00

--- src/components/ap.jsx
+++ src/components/ap.jsx
@@ -1,61 +1,3 @@
-import React from 'react';
-import { Component } from 'react';
-import { ContactForm } from './ContactForm';
-import { ContactList } from './ContactList';
-import { Filter } from './Filter';
-import { StyledWrapper } from './App.styled';
-
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  handleAddContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  handleFilterChange = e => {
-    this.setState({ filter: e.target.value.toLowerCase() });
-  };
-  handleDeleteUser = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contacts => contacts.id !== id),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
-
-    return (
-      <StyledWrapper>
-        <h1>Phone Book</h1>
-        <ContactForm
-          onAddContact={this.handleAddContact}
-          contacts={this.state.contacts}
-        />
-        <h2>Contacts</h2>
-        <Filter filter={filter} onChange={this.handleFilterChange} />
-        <ContactList
-          contacts={filteredContacts}
-          onClickDelete={this.handleDeleteUser}
-        />
-      </StyledWrapper>
-    );
-  }
-}
-// =======================================
-
 // import React from 'react';
 
 // import { nanoid } from 'nanoid';
@@ -150,7 +92,7 @@ export class App extends Component {
 //         <ContactList>
 //           {filteredContacts.map(contact => (
 //             <StyledLi key={contact.id}>
-//               {contact.name} - {contact.number.toString()}
+//               {contact.name} - {contact.number}
 //             </StyledLi>
 //           ))}
 //         </ContactList>
